9d80ecafd609380f893e15f8cb1a56324ca7d82e
Péter Attila Rigó <rigopeti96@gmail.com>
2023-10-10 23:25:08 +0200

    permission request works but not goes to act pos

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:homework/createuser.dart';
 import 'package:homework/login.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const TraWellApp());
@@ -27,14 +29,73 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   late GoogleMapController mapController;
+//  LocationService locationService = LocationService();
+//  late LatLng actualLocation;
+
   List<Page> pages = [
     const MaterialPage(child: TraWellApp()),
   ];
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
+
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+  LatLng getActPosition(){
+    Future<Position> actPosFuture = _determinePosition();
+    actPosFuture.then((value){
+      return LatLng(value.latitude, value.longitude);
+    });
+
+    return LatLng(0, 0);
+  }
+
+
+  /*LatLng getActPosition(){
+    Future<LatLng> actPositionFuture = locationService.getCurrentLocation();
+    actPositionFuture.then((value) {
+      actualLocation = value;
+    });
+
+    return actualLocation;
+  }*/
 
   void _navigateToNextScreen() {
     Navigator.of(context).push(
@@ -62,7 +123,7 @@ class _MainPageState extends State<MainPage> {
             GoogleMap(
               onMapCreated: _onMapCreated,
               initialCameraPosition: CameraPosition(
-                target: _center,
+                target: getActPosition(),
                 zoom: 11.0,
               ),
             ),
