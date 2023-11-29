342abf29abb838dc731f426722776c999ad71a72
Péter Attila Rigó <rigopeti96@gmail.com>
2023-11-29 09:30:57 +0100

    createerrorresponse.dart data classes set

--- lib/alerts/createerrorresponse.dart
+++ lib/alerts/createerrorresponse.dart
@@ -1,25 +1,40 @@
 class CreateErrorResponse {
-  final String name;
-  final String username;
-  final String password;
-  final String email;
-  final Set<String> roles;
+  final String id;
+  final String reportType;
+  final String stationName;
+  final String transportType;
+  final double latitude;
+  final double longitude;
+  final DateTime reportDate;
+  final DateTime reportDateUntil;
+  final String reporterName;
+  final String modifierName;
 
   const CreateErrorResponse({
-    required this.name,
-    required this.username,
-    required this.password,
-    required this.email,
-    required this.roles,
+    required this.id,
+    required this.reportType,
+    required this.stationName,
+    required this.transportType,
+    required this.latitude,
+    required this.longitude,
+    required this.reportDate,
+    required this.reportDateUntil,
+    required this.reporterName,
+    required this.modifierName,
   });
 
   factory CreateErrorResponse.fromJson(Map<String, dynamic> json) {
     return CreateErrorResponse(
-      name: json['name'] as String,
-      username: json['username'] as String,
-      password: json['password'] as String,
-      email: json['email'] as String,
-      roles: json['roles'] as Set<String>,
+      id: json['id'] as String,
+      reportType: json['reportType'] as String,
+      stationName: json['stationName'] as String,
+      transportType: json['transportType'] as String,
+      latitude: json['latitude'] as double,
+      longitude: json['longitude'] as double,
+      reportDate: json['reportDate'] as DateTime,
+      reportDateUntil: json['reportDateUntil'] as DateTime,
+      reporterName: json['reporterName'] as String,
+      modifierName: json['modifierName'] as String,
     );
   }
 }
